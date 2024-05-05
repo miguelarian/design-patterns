f3e62f2b68aa17ef8236af92550af8cf7f43ce1f
miguelarian <miguelvr88@gmail.com>
2024-05-05 17:15:59 +0200

    add single threaded naive singleton

---
+++ Singleton/src/main/java/com/miguelvela/Database.java
@@ -0,0 +1,27 @@
+package com.miguelvela;
+
+import java.util.List;
+
+public class Database {
+    private List<String> data;
+    private static Database instance;
+
+    private Database(List<String> data) {
+        this.data = data;
+    }
+
+    public static Database getInstance(List<String> data) {
+        if(instance != null)
+        {
+            return  instance;
+        }
+
+        instance = new Database(data);
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ", this.data);
+    }
+}
