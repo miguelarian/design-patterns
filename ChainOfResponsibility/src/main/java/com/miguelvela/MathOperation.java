b068d153f4550398a1e012bf1a97b911b8ce8bfb
miguelarian <miguelvr88@gmail.com>
2024-05-21 14:24:44 +0200

    change operator types to double to support divs

--- ChainOfResponsibility/src/main/java/com/miguelvela/MathOperation.java
+++ ChainOfResponsibility/src/main/java/com/miguelvela/MathOperation.java
@@ -1,21 +1,21 @@
 package com.miguelvela;
 
 public class MathOperation {
-    private int operator1;
-    private int operator2;
+    private double operator1;
+    private double operator2;
     private String operation;
 
-    public MathOperation(int number1, int number2, String operation) {
+    public MathOperation(double number1, double number2, String operation) {
         this.operator1 = number1;
         this.operator2 = number2;
         this.operation = operation;
     }
 
-    public int getOperator1() {
+    public double getOperator1() {
         return operator1;
     }
 
-    public int getOperator2() {
+    public double getOperator2() {
         return operator2;
     }
 
